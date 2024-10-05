b4f92413c8bf080cdba8f56b909824c160fd0c90
Jyotsna Bhatia <69782687+Jyotsna03@users.noreply.github.com>
2024-10-05 14:37:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.py
@@ -0,0 +1,46 @@
+#User function Template for python3
+class Solution:
+	def twoSum(self,arr, target):
+	    arr.sort()
+	    h = 0
+	    l = 0
+	    r = len(arr) - 1
+	    while l < r:
+	        add = arr[l] + arr[r]
+	        if add == target :
+	            h = 1
+	            return "true"
+	        elif add > target :
+	            r -=1
+	        else:
+	            l +=1
+        if (h==1):         
+	        return "false"          
+	    
+		# code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+    T = int(input())
+    while T > 0:
+        x = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.twoSum(arr, x)
+        if ans:
+            print("true")
+        else:
+            print("false")
+        T -= 1
+        #print("~")
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
