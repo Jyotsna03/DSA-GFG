58790d0535987946eef0b04a78174a6b12728fbb
Jyotsna Bhatia <69782687+Jyotsna03@users.noreply.github.com>
2024-10-19 12:34:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.py
+++ Difficulty: Easy/Second Largest/second-largest.py
@@ -1,20 +1,13 @@
 #User function Template for python3
-from typing import List
-
 class Solution:
-    def print2largest(self, arr: List[int]) -> int:
-        # Convert array to set to get distinct elements
-        distinct_elements = list(set(arr))
-        
-        # Check if there are at least two distinct elements
-        if len(distinct_elements) < 2:
-            return -1
-        
-        # Sort the distinct elements in descending order
-        distinct_elements.sort(reverse=True)
-        
-        # Return the second largest element
-        return distinct_elements[1]
+    def getSecondLargest(self, arr):
+        # Code Here
+        arr=list(set(arr))
+        arr.sort()
+        if len(arr)==1:
+            return "-1"
+        else:
+            return arr[-2]
 
 
 #{ 
@@ -26,7 +19,7 @@ if __name__ == "__main__":
     for _ in range(t):
         arr = list(map(int, input().split()))
         ob = Solution()
-        ans = ob.print2largest(arr)
+        ans = ob.getSecondLargest(arr)
         print(ans)
 
 # } Driver Code Ends
