425bda0905feed05bf5237965261e06af0287965
Jyotsna Bhatia <69782687+Jyotsna03@users.noreply.github.com>
2024-11-24 11:44:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.py
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.py
@@ -1,24 +1,18 @@
 #User function Template for python3
+#User function Template for python3
 
 class Solution:
-    # Function to find the sum of contiguous subarray with maximum sum.
-    def maxSubArraySum(self, arr):
-        n = len(arr)
-        current_sum = 0
-        max_sum = arr[0]
-        
-        for i in range(n):
-            current_sum += arr[i]
-            max_sum = max(max_sum, current_sum)
-            
-            # Reset current_sum if it goes below 0
-            if current_sum < 0:
-                current_sum = 0
-        
-        return max_sum
-
-            
+    ##Complete this function
+    #Function to find the sum of contiguous subarray with maximum sum.
+    def maxSubArraySum(self,arr):
         ##Your code here
+        res = arr[0]
+        maxEnding = arr[0]
+        for i in range(1, len(arr)):
+            maxEnding = max(maxEnding + arr[i], arr[i])
+            res = max(res, maxEnding)
+        return res
+
 
 
 #{ 
